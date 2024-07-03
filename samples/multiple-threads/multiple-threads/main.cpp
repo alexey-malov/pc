e39ecfffc8fff096aa612866c245fe0d2c203755
Alexey Malov <malov.aleksey@gmail.com>
2024-07-04 01:53:13 +0300

    Add multiple threads sample

---
+++ samples/multiple-threads/multiple-threads/main.cpp
@@ -0,0 +1,17 @@
+﻿#include <thread>
+#include <syncstream>
+#include <iostream>
+
+int main()
+{
+	std::osyncstream syncOut{std::cout};
+	std::jthread worker1{[&syncOut] {
+		syncOut << "Hello" << std::endl;
+	}};
+	
+	std::jthread worker2{[&syncOut] {
+		syncOut << " world" << std::endl;
+	}};
+
+	return 0;
+}
