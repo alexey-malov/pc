1afe66db29057f532c7e76b2803ebe82ceb02482
Alexey Malov <alexey.malov@ispring.com>
2025-03-12 15:57:21 +0300

    Реализовал проигрывание звука на частоте 40 кГц

--- labs/04/samples/audio_player/audio_player.cpp
+++ labs/04/samples/audio_player/audio_player.cpp
@@ -1,11 +1,34 @@
-﻿#define MINIAUDIO_IMPLEMENTATION
-#include "miniaudio.h"
+﻿#include "miniaudio.h"
 
 #include <functional>
 #include <iostream>
+#include <span>
+#include <string>
+#include <cmath>
+#include <numbers>
 
 namespace audio
 {
+
+inline const std::error_category& ErrorCategory()
+{
+	class AudioErrorCategory : public std::error_category
+	{
+	public:
+		const char* name() const noexcept override
+		{
+			return "Audio error category";
+		}
+
+		std::string message(int errorCode) const override
+		{
+			return ma_result_description(static_cast<ma_result>(errorCode));
+		}
+	};
+
+	static AudioErrorCategory errorCategory;
+	return errorCategory;
+}
 
 class Device
 {
@@ -23,6 +46,32 @@ public:
 		{
 			throw std::runtime_error("Device initialization failed");
 		}
+	}
+
+	void Start()
+	{
+		if (auto result = ma_device_start(&m_device); result != MA_SUCCESS)
+		{
+			throw std::system_error(result, ErrorCategory());
+		}
+	}
+
+	void Stop()
+	{
+		if (auto result = ma_device_stop(&m_device); result != MA_SUCCESS)
+		{
+			throw std::system_error(result, ErrorCategory());
+		}
+	}
+
+	ma_device* operator->() noexcept
+	{
+		return &m_device;
+	}
+
+	const ma_device* operator->() const noexcept
+	{
+		return &m_device;
 	}
 
 	void SetDataCallback(DataCallback dataCallback)
@@ -49,32 +98,93 @@ private:
 	DataCallback m_dataCallback;
 };
 
-class Player
+class SoundGenerator
 {
 public:
-	Player(ma_format format, int channels)
-		: m_device(CreateConfig(format, channels))
+	using DataCallback = std::function<void(void* output, ma_uint32 frameCount)>;
+
+	SoundGenerator(ma_format format, ma_uint32 channels, ma_uint32 sampleRate = 48000)
+		: m_device(CreateConfig(format, channels, sampleRate))
 	{
+		m_device.SetDataCallback([this](void* output, const void*, ma_uint32 frameCount) {
+			if (m_dataCallback)
+			{
+				m_dataCallback(output, frameCount);
+			}
+		});
+	}
+
+	void Start()
+	{
+		m_device.Start();
+	}
+
+	void Stop()
+	{
+		m_device.Stop();
+	}
+
+	ma_format GetFormat() const noexcept
+	{
+		return m_device->playback.format;
+	}
+
+	ma_uint32 GetChannels() const noexcept
+	{
+		return m_device->playback.channels;
+	}
+
+	ma_uint32 GetSampleRate() const noexcept
+	{
+		return m_device->sampleRate;
+	}
+
+	void SetDataCallback(DataCallback callback)
+	{
+		m_dataCallback = std::move(callback);
 	}
 
 private:
-	static ma_device_config CreateConfig(ma_format format, int channels)
+	static ma_device_config CreateConfig(ma_format format, ma_uint32 channels, ma_uint32 sampleRate)
 	{
 		auto config = ma_device_config_init(ma_device_type_playback);
 		auto& playback = config.playback;
 
 		playback.format = format;
 		playback.channels = channels;
-		config.sampleRate = 44100;
+		config.sampleRate = sampleRate;
+
 		return config;
 	}
 
+	DataCallback m_dataCallback;
 	Device m_device;
 };
 } // namespace audio
 
 int main()
 {
+	audio::SoundGenerator generator(ma_format_f32, 1);
+	// Частота ноты Ля 1 октавы
+	constexpr double frequency = 440.0;
+	const auto period = generator.GetSampleRate() / frequency;
 
-	return 0;
+	generator.SetDataCallback([phase = 0.0, period](void* output, ma_uint32 frameCount) mutable {
+		auto samples = std::span(static_cast<ma_float*>(output), frameCount);
+		for (auto& sample : samples)
+		{
+			// Синусоидальный сигнал (чистый тон)
+			sample = static_cast<ma_float>(std::sin(phase * 2 * std::numbers::pi / period));
+
+			if ((phase += 1) >= period)
+			{
+				phase -= period;
+			}
+		}
+	});
+
+	generator.Start();
+
+	std::string s;
+	std::getline(std::cin, s);
 }
