48f923e6619c5f49386379dde613175b21b3d240
Alexey Malov <alexey.malov@ispring.com>
2025-03-13 12:24:09 +0300

    Доработал звуковой пример, чтобы проиллюстрировать проигрывание аккорда

--- labs/04/samples/audio_player/audio_player.cpp
+++ labs/04/samples/audio_player/audio_player.cpp
@@ -1,11 +1,11 @@
 ﻿#include "miniaudio.h"
 
+#include <cmath>
 #include <functional>
 #include <iostream>
+#include <numbers>
 #include <span>
 #include <string>
-#include <cmath>
-#include <numbers>
 
 namespace audio
 {
@@ -98,12 +98,12 @@ private:
 	DataCallback m_dataCallback;
 };
 
-class SoundGenerator
+class Player
 {
 public:
 	using DataCallback = std::function<void(void* output, ma_uint32 frameCount)>;
 
-	SoundGenerator(ma_format format, ma_uint32 channels, ma_uint32 sampleRate = 48000)
+	Player(ma_format format, ma_uint32 channels, ma_uint32 sampleRate = 48000)
 		: m_device(CreateConfig(format, channels, sampleRate))
 	{
 		m_device.SetDataCallback([this](void* output, const void*, ma_uint32 frameCount) {
@@ -160,30 +160,66 @@ private:
 	DataCallback m_dataCallback;
 	Device m_device;
 };
+
+class SineWaveGenerator
+{
+public:
+	SineWaveGenerator(ma_uint32 sampleRate, ma_float frequency, ma_float amplitude = 1.f, ma_float startPhase = 0.f)
+		: m_sampleRate{ sampleRate }
+		, m_frequency{ frequency }
+		, m_amplitude{ amplitude }
+		, m_phase{ startPhase }
+	{
+	}
+	ma_float GetNextSample()
+	{
+		constexpr auto twoPi = static_cast<ma_float>(2.f * std::numbers::pi);
+
+		auto sample = m_amplitude * std::sin(m_phase);
+		m_phase = std::fmod(m_phase + m_phaseShift, twoPi);
+
+		return sample;
+	}
+
+private:
+	ma_uint32 m_sampleRate;
+	ma_float m_frequency;
+	ma_float m_amplitude;
+	ma_float m_phase;
+	// На сколько смещается фаза синусоидальных колебаний при переходе к следующему отсчёту
+	ma_float m_phaseShift = static_cast<ma_float>(2.f * std::numbers::pi * m_frequency / m_sampleRate);
+};
+
 } // namespace audio
 
 int main()
 {
-	audio::SoundGenerator generator(ma_format_f32, 1);
-	// Частота ноты Ля 1 октавы
-	constexpr double frequency = 440.0;
-	const auto period = generator.GetSampleRate() / frequency;
+	audio::Player player(ma_format_f32, 1);
 
-	generator.SetDataCallback([phase = 0.0, period](void* output, ma_uint32 frameCount) mutable {
+	// Частота звука, который выше на 1 полутон, выше в корень 12 степени из 2 раз.
+	// Частота звука, который выше на 1 октаву (12 полутонов) выше в 2 раза
+
+	// Аккорд Ля минор A4-C5-E5
+	// Ля 1 октавы
+	audio::SineWaveGenerator a4{ player.GetSampleRate(), 440.0f, 0.3f }; // Ля 1 октавы
+	// Нота До 2 октавы (на 3 полутона выше, чем Ля 1 октавы)
+	audio::SineWaveGenerator c5{ player.GetSampleRate(), 440.0f * std::powf(2.0f, 3.0f / 12.f), 0.3f };
+	// Нота Ми 2 октавы (на 7 полутонов выше, чем Ля 1 октавы)
+	audio::SineWaveGenerator e5{ player.GetSampleRate(), 440.0f * std::powf(2.0f, 7.0f / 12.f), 0.3f };
+
+	player.SetDataCallback([&a4, &c5, &e5](void* output, ma_uint32 frameCount) mutable {
+		// Заполняем буфер аудио-сэмплами
 		auto samples = std::span(static_cast<ma_float*>(output), frameCount);
 		for (auto& sample : samples)
 		{
-			// Синусоидальный сигнал (чистый тон)
-			sample = static_cast<ma_float>(std::sin(phase * 2 * std::numbers::pi / period));
-
-			if ((phase += 1) >= period)
-			{
-				phase -= period;
-			}
+			// Складываем сэмплы со всех генераторов аудио-сигнала
+			sample = a4.GetNextSample();
+			sample += c5.GetNextSample();
+			sample += e5.GetNextSample();
 		}
 	});
 
-	generator.Start();
+	player.Start();
 
 	std::string s;
 	std::getline(std::cin, s);
