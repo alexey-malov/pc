aa778a76c5308c5387d67cdd430f2865516bdb15
Alexey Malov <malov.aleksey@gmail.com>
2025-05-05 20:33:25 +0300

    Добавил материалы по корутинам

--- samples/coroutines/07_task/07_task.cpp
+++ samples/coroutines/07_task/07_task.cpp
@@ -2,18 +2,21 @@
 #include "sync_wait.h"
 #include <iostream>
 
-Task<int> Inner()
+Task<int> Add(int x, int y)
 {
 	std::cout << "Enter inner\n";
-	co_return 42;
+	co_return x + y;
 }
 
 Task<void> Outer()
 {
 	std::cout << "Enter outer\n";
-	int result = co_await Inner();
+	auto r1 = Add(10, 20);
+	auto r2 = Add(6, 6);
+	auto result = (co_await r1) + (co_await r2);
 	std::cout << result << "\n";
 	std::cout << "Exit outer\n";
+	co_return;
 }
 
 int main()
--- samples/coroutines/10_continuation/10_continuation.cpp
+++ samples/coroutines/10_continuation/10_continuation.cpp
@@ -1,5 +1,4 @@
-﻿#if 1
-
+﻿#include <cassert>
 #include <coroutine>
 #include <iostream>
 
@@ -7,7 +6,7 @@ struct Task
 {
 	struct promise_type
 	{
-		std::coroutine_handle<> continuation;
+		std::coroutine_handle<> continuation; // Хендл корутины для продолжения
 
 		Task get_return_object()
 		{
@@ -19,21 +18,20 @@ struct Task
 		{
 			struct Awaiter
 			{
-				std::coroutine_handle<> continuation;
 				bool await_ready() noexcept { return false; }
-				void await_suspend(std::coroutine_handle<>) const noexcept
+				void await_suspend(std::coroutine_handle<promise_type> coro) const noexcept
 				{
-					if (continuation)
-						continuation.resume();
+					if (auto next = coro.promise().continuation)
+						next.resume();
 				}
 				void await_resume() noexcept {}
 			};
-			return Awaiter{ continuation };
+			return Awaiter{};
 		}
 		void return_void() {}
 		void unhandled_exception()
 		{
-			std::terminate();
+			std::terminate(); // Для примера
 		}
 		~promise_type()
 		{
@@ -85,124 +83,10 @@ int main()
 
 	// Устанавливаем continuation: после завершения first возобновляется second
 	first.SetContinuation(second.handle);
+	std::cout << "resume first\n";
 	first.Resume(); // Запускаем первую корутину
 	first.Resume();
-}
-
-
-#else
-#include <coroutine>
-#include <iostream>
-#include <utility>
-
-struct ReturnObject
-{
-	struct promise_type
-	{
-		#if 0
-		struct FinalAwaiter
-		{
-			bool await_ready() const noexcept
-			{
-				return false; // возвращаем false
-			}
-			std::coroutine_handle<> await_suspend(std::coroutine_handle<promise_type> coro) noexcept
-			{
-				return coro.promise().continuation;
-			}
-			void await_resume() noexcept {}
-		};
-		#endif
-
-		ReturnObject get_return_object()
-		{
-			std::cout << "get_return_object\n";
-			return { std::coroutine_handle<promise_type>::from_promise(*this) };
-		}
-		std::suspend_never initial_suspend() { return {}; }
-		std::suspend_always final_suspend() noexcept { return {}; }
-		void return_void() {}
-		void unhandled_exception() {}
-
-		std::coroutine_handle<> continuation;
-	};
-
-	ReturnObject(std::coroutine_handle<promise_type> h) noexcept
-		: handle(h)
-	{
-	}
-
-	ReturnObject(ReturnObject&& other) noexcept
-		: handle(std::exchange(other.handle, nullptr))
-	{
-	}
-
-	ReturnObject(const ReturnObject&) = delete;
-
-	#if 0
-	auto operator co_await() const noexcept
-	{
-		struct Awaitable
-		{
-			bool await_ready() const noexcept
-			{
-				return !coroutine || !coroutine.done();
-			}
-			std::coroutine_handle<> await_suspend(std::coroutine_handle<promise_type> coro) noexcept
-			{
-				coro.promise().continuation = coro;
-				return coro.promise().continuation;
-			}
-			void await_resume() noexcept {}
-
-			std::coroutine_handle<promise_type> coroutine;
-		};
-		return Awaitable{};
-	}
-	#endif
-
-	~ReturnObject()
-	{
-		if (handle)
-			handle.destroy();
-	}
-
-	std::coroutine_handle<promise_type> handle;
-};
-
-struct SimpleAwaitable
-{
-	bool await_ready() const noexcept
-	{
-		std::cout << "await_ready (false)\n";
-		return false; // возвращаем false
-	}
-
-	// Приостанавливает выполнение текущей корутины
-	void await_suspend(std::coroutine_handle<ReturnObject::promise_type> coro) const noexcept
-	{
-		coro.promise().continuation = coro; // сохраняем корутину
-		std::cout << "await_suspend\n";
-	}
-
-	int await_resume() const noexcept
-	{
-		std::cout << "await_resume: 42\n";
-		return 42; // возвращаем значение из co_await
-	}
-};
-
-ReturnObject DoSomething()
-{
-	int result = co_await SimpleAwaitable{};
-	std::cout << "co_await returned: " << result << '\n';
-}
-
-int main()
-{
-	auto ret = DoSomething();
-	std::cout << "resuming\n";
-	ret.handle.resume();
+	assert(first.handle.done());
+	assert(second.handle.done());
 	std::cout << "exit from main\n";
 }
-#endif
